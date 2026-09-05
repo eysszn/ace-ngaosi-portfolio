c0cce97a1ed3d13f0dfa51d00fcd69c5a44a7ee5
eysszn <azngaosi2004@gmail.com>
2026-09-05 11:31:57 +0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server :{
+    open: true
+  },
+  plugins: [react(), tailwindcss()],
+})
